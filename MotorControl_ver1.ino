1addefe7a813c1601881a343f6cc6a1f3c5830d6
songky08 <songky08@gmail.com>
2016-05-16 19:26:44 +0900

    added API of each Motors

--- MotorControl_ver1.ino
+++ MotorControl_ver1.ino
@@ -1,25 +1,56 @@
-void motor_initialize() {
-  // put your setup code here, to run once:
-  pinMode (9, OUTPUT) ;
-  pinMode (10, OUTPUT) ;
+void motor_initialize(){
+  int currenttime=millis();
+  pinMode (9, OUTPUT) ; //motor 1
+  pinMode (10, OUTPUT) ; //motor 2
   TCCR1A = 0xA2 ;  // Set up mode 14, fast PWM with TOP=ICR1
   TCCR1B = 0x19 ;   // prescale is divide-by-1, so 2000 cycles = 125us
   ICR1  = 2000-1 ;    // counts from 0..1999 continuously
-  OCR1A = 1500-1 ;  // set PWM duty
-  OCR1B = 1500-1 ;
-  pinMode (5, OUTPUT) ;
+  OCR1A = 1999 ;  // min = 40, max = 1999;
+  OCR1B = 1999 ; // min = 40, max = 1999;
+  pinMode (5, OUTPUT) ; // motor 3
   TCCR0A = 0xA3 ;   //Set up mode fast PWM with TOP = OCR0A
   TCCR0B = 0x1A ;
   OCR0A = 249 ;  // set frequency at 8Khz
   OCR0B = 0 ;   // min = 3, max = 255
-  pinMode (3, OUTPUT) ;
+  pinMode (3, OUTPUT) ; // motor 4
   TCCR2A = 0xA3 ;   //Set up mode fast PWM with TOP = OCR0A
   TCCR2B = 0x1A ;
   OCR2A = 249 ;  // set frequency at 8Khz
   OCR2B = 0 ;   // min = 3, max = 255
+
+  while(1)
+  {
+    if(millis()>currenttime+1000)
+    break;
+  }
+  OCR1A=40;
+  OCR1B=40;
+  OCR0B=0;
+  OCR2B=0;
 }
 
-void motor_activate(int command)
+void motor_write1( float v)
 {
-      
+    OCR1A=v*1959/255 + 40;
+}
+
+
+void motor_write2( float v)
+{
+    OCR1B=v*1959/255 + 40;
+}
+
+
+void motor_write3( float v)
+{
+    OCR0B=v;
+}
+
+
+void motor_write4( float v)
+{
+    OCR2B=v;
+}
+
+void loop(){
 }
